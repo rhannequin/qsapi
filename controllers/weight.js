45760560e80bd1be518e42d0f3a99f35c5b1e8e7
Rémy Hannequin <remy.hannequin@gmail.com>
2014-01-11 22:48:20 +0100

    Use async to remove Pyramid of Doom in Weights controller

--- controllers/weight.js
+++ controllers/weight.js
@@ -1,61 +1,85 @@
-var Util = require('../utils/util');
+var Util = require('../utils/util')
+  , async = require('async')
 
 module.exports = function(app) {
 
   var db = app.get('db')[0]
     , Weight = require('../models/Weight')(db)
     , User = require('../models/User')(db)
-    , routes = {};
+    , routes = {}
 
   routes.list = function(req, res, next) {
-    Weight.findAll({}, function (err, weights) {
+    Weight.findAll({}, function(err, weights) {
       Util.checkErrors(err, res, null, function() {
-        res.send(weights);
-      });
-    });
-  };
+        res.send(weights)
+      })
+    })
+  }
 
   routes.show = function(req, res, next) {
-    Weight.findOne({code: req.params.weightId}, function (err, weight) {
+    Weight.findOne({code: req.params.weightId}, function(err, weight) {
       Util.checkErrors(err, res, null, function() {
-        res.send(weight);
-      });
-    });
-  };
+        res.send(weight)
+      })
+    })
+  }
 
   routes.insert = function(req, res, next) {
-    var dataToInsert = req.body;
-    dataToInsert.code = Util.generateCode();
-    dataToInsert.created_at = new Date();
-    User.findOne({code: req.params.userId}, function (err, user) {
-      Util.checkErrors(err, res, null, function() {
-        dataToInsert.author = {
-          code : user.code,
-          email : user.email,
-          username : user.username
-        };
+    var dataToInsert = req.body
+    dataToInsert.code = Util.generateCode()
+    dataToInsert.created_at = new Date()
 
-        Weight.create(dataToInsert, {}, function(err) {
-          Util.checkErrors(err, res, null, function() {
-            Weight.findOne({code: dataToInsert.code}, function (err, weight) {
-              Util.checkErrors(err, res, null, function() {
-                res.status(201).send(weight);
-              });
-            });
-          });
-        });
-      });
-    });
-  };
+    async.waterfall([
+        _initIsert
+      , _getUser
+      , _fillData
+      , _createWeight
+      , _getWeight
+    ], function(err, weight) {
+      if(err) return res.status(500).send({error: err})
+      return res.status(201).send(weight)
+    })
+  }
 
   routes['delete'] = function(req, res, next) {
     Weight.remove({code: req.params.weightId}, function (err, result){
       Util.checkErrors(err, res, null, function() {
-        res.status(200).send({result: 'deleted'});
-      });
-    });
-  };
+        res.status(200).send({result: 'deleted'})
+      })
+    })
+  }
 
-  return routes;
 
-};
+  function _getUser(code, dataToInsert, cb) {
+    User.findOne({code: code}, function(err, user) {
+      if(err) return cb(err)
+      cb(null, user, dataToInsert)
+    })
+  }
+
+  function _fillData(user, dataToInsert, cb){
+    dataToInsert.author = {
+        code : user.code
+      , email : user.email
+      , username : user.username }
+    cb(null, dataToInsert)
+  }
+
+  function _createWeight(dataToInsert, cb) {
+    Weight.create(dataToInsert, {}, function(err) {
+      if(err) return cb(err, dataToInsert)
+      cb(null, dataToInsert.code)
+    })
+  }
+
+  function _getWeight(code, cb) {
+    Weight.findOne({code: code}, function(err, weight) {
+      if(err) return cb(err)
+      cb(null, weight)
+    })
+  }
+
+
+  return routes
+
+}
