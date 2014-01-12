88360918282fb25b74c77714540312444e3fca52
Rémy Hannequin <remy.hannequin@gmail.com>
2014-01-12 17:35:58 +0100

    Add comments to User model

--- models/User.js
+++ models/User.js
@@ -1,42 +1,51 @@
-var Util = require('../utils/util');
+// Require Util files
+var Util = require('../utils/util')
 
+// Define the module with the database connection instance as parameter
 module.exports = function(db) {
 
-  // Get database
+    // Get database
   var c = db.collection('users')
-    , User = {};
+    // Define the returned object
+    , User = {}
 
+  // Get a list of User
   User.findAll = function(params, cb) {
-    c.find(params).toArray(cb);
-  };
+    c.find(params).toArray(cb)
+  }
 
+  // Get one single User
   User.findOne = function(params, cb) {
     c.findOne(params, function(err, user) {
-      if (err) return cb({error: 500});
-      if (user === null) return cb({error: 404});
-      return cb(null, user);
-    });
-  };
+      if (err) return cb({error: 500})
+      if (user === null) return cb({error: 404})
+      return cb(null, user)
+    })
+  }
 
+  // Add a User
   User.create = function(params, options, cb) {
     // Check if request is correct
-    Util.checkRequiredParams(params, ['username','password','email'], cb);
+    Util.checkRequiredParams(params, ['username','password','email'], cb)
     // Check if user already exists
-    var p = { username: params.username, $or: [{ email: params.email }] };
+    var p = { username: params.username, $or: [{ email: params.email }] }
     User.findAll(p, function(err, user) {
-      if(user.length) return cb({error: 409});
-      c.insert(params, options, cb);
-    });
-  };
+      if(user.length) return cb({error: 409})
+      c.insert(params, options, cb)
+    })
+  }
 
+  // Update a User
   User.edit = function(params, update, options, cb) {
-    c.update(params, update, options, cb);
-  };
+    c.update(params, update, options, cb)
+  }
 
+  // Delete a User
   User.remove = function(params, cb) {
-    c.remove(params, cb);
-  };
+    c.remove(params, cb)
+  }
 
-  return User;
+  // Return the object filled with methods
+  return User
 
-};
+}
